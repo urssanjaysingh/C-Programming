db734248792e2f279c97662bacae68eebf5f85a1
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-03-04 21:58:02 +0530

    Changes

--- 013. ifElse.c
+++ 013. ifElse.c
@@ -6,12 +6,12 @@ int main(){
     printf("Enter your age: ");
     scanf("%d",&age);
 
-    if(age >= 18){
+    if(age > 120 || age < 0)
+        printf("Invalid Age");
+    else if(age >= 18)
         printf("You are eligible to vote");
-    }
-    else{
+    else
         printf("Sorry, you are not eligible to vote");
-    }
 
     return 0;
 }
